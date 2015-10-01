50ad976bc0e8767df06b6caa321a944b8470322a
Jeremy Ward <jerskiward@gmail.com>
2015-09-30 20:32:41 -0600

    completed add-recipe scripts to send recipe to server, added angular

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,7 +1,29 @@
 //=== app to send recipe to server for recording ==========
 
+//=== function to get list items into array ===============
+function getIngredients(){
+	var x = document.getElementsByClassName("ingr"),
+		y=[];
+	for(var i=0, z=x.length;i<z;i++){
+		y.push(x[i].value);
+	}
+	return y;
+}
+
+//=== Define the application ======================
 var app=angular.module('add-app',[]);
-
+//=== Set up the controlller ======================
 app.controller('main-controller', function($scope, $http){
-
+	$scope.sameRecipe=false;
+	$scope.sendRecipe=function(){
+		console.log("Send a recipe");
+		$scope.recipe.ingredients=getIngredients();
+		console.log($scope.recipe.ingredients);
+		$http.post("/api/add",$scope.recipe)
+			.success(function(data){
+				if(!data)
+					$scope.sameRecipe=true;
+				window.location.href="/recipe/"+data.recipeID;	
+			});
+	}
 });
